1568a6b053fe0770bcaffa273a7595c076b023ea
saichittala <sai.chittala@gmail.com>
2024-11-16 05:44:43 +0530

    Web New Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
+import CategoryPage from './pages/CategoryPage';
+import ProductDetailPage from './pages/ProductDetailPage';
 
-const CategoryPage = React.lazy(() => import('./pages/CategoryPage'));
-const ProductDetailPage = React.lazy(() => import('./pages/ProductDetailPage'));
-
+// Define the App component
 const App = () => {
   return (
-    <Router basename={process.env.PUBLIC_URL}>  {/* Add basename here */}
+    <Router>
       <div className="min-h-screen bg-white">
         <Toaster position="top-center" />
         <Navigation />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/category/:gender" element={<CategoryPage />} />
-            <Route path="/product/:id" element={<ProductDetailPage />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category/:gender" element={<CategoryPage />} />
+          <Route path="/product/:id" element={<ProductDetailPage />} />
+        </Routes>
       </div>
     </Router>
   );
 };
 
+// Export App as a default export
 export default App;
